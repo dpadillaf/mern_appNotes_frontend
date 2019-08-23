a8457045b07bec3aabf3cb1fca43e9dbbac5517b
David <dpadillaf@unicartagena.edu.co>
2019-08-23 16:29:04 -0500

    frontend terminado

--- src/components/CreateNote.js
+++ src/components/CreateNote.js
@@ -10,18 +10,44 @@ export default class CreateNote extends Component {
         userSelected: '',
         title: '',
         content: '',
-        date: new Date()
+        date: new Date(),
+        editing: false,
+        _id: ''
     }
 
     async componentDidMount(){
         const res = await axios.get( 'http://localhost:4000/api/users' );
-        this.setState( { users: res.data.map( user => user.username ) } );
-        console.log( this.state.users );
+        this.setState( { 
+            users: res.data.map( user => user.username ),
+            userSelected: res.data[0].username
+         } );
+         if ( this.props.match.params.id ){
+             const res = await axios.get( 'http://localhost:4000/api/notes/' + this.props.match.params.id );
+             this.setState( {
+                 title: res.data.title,
+                 content: res.data.content,
+                 date: new Date( res.data.date ),
+                 userSelected: res.data.author,
+                 editing: true,
+                 _id: this.props.match.params.id
+             } );
+         }
     }
 
     onSubmit = async e => {
         e.preventDefault();
-        console.log(this.state.userSelected, this.state.title, this.state.content)
+        const newNote = {
+            title: this.state.title,
+            content: this.state.content,
+            date: this.state.date,
+            author: this.state.userSelected
+        }
+        if ( this.state.editing ){
+            await axios.put( 'http://localhost:4000/api/notes/' + this.state._id, newNote );
+        }else{
+            await axios.post( 'http://localhost:4000/api/notes', newNote );
+        }
+        window.location.href = '/';
     }
 
     onInputChange = e => {
@@ -48,11 +74,12 @@ export default class CreateNote extends Component {
                             <select 
                                 name="userSelected"
                                 className="form-control"
-                                onChange={ this.onInputChange } >
+                                onChange={ this.onInputChange }
+                                value={ this.state.userSelected } >
 
                                     {
                                         this.state.users.map( user => 
-                                            <option key={ user } value={ user }>
+                                            <option key={ user } value={ user } >
                                                 { user }
                                             </option>
                                             )
@@ -68,6 +95,7 @@ export default class CreateNote extends Component {
                                 placeholder="Title"
                                 name="title"
                                 onChange={ this.onInputChange }
+                                value={ this.state.title }
                                 required
                                 />
                         </div>
@@ -77,7 +105,8 @@ export default class CreateNote extends Component {
                                 name="content" 
                                 className="form-control" 
                                 placeholder="Content"
-                                onChange={ this.onInputChange } 
+                                onChange={ this.onInputChange }
+                                value={ this.state.content } 
                                 required
                                 >    
                             </textarea>
@@ -88,6 +117,7 @@ export default class CreateNote extends Component {
                                 className="form-control"
                                 selected={ this.state.date }
                                 onChange={ this.onDateChange }
+                                value={ this.state.date }
                             />
                         </div>
 
